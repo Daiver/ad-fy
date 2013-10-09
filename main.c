271f8711de672635e44e78202e1ccfca57869f4c
Daiver <ra22341@ya.ru>
2013-10-09 13:43:19 +0400

    parsing without tabs; minor fixes

--- main.c
+++ main.c
@@ -8,7 +8,7 @@
 
 struct TNode
 {
-    char *name;
+    const char *name;
     unsigned int childs_length;
     struct TNode *childs;
 }; 
@@ -24,6 +24,16 @@ typedef struct TTokensStream TokensStream;
 const char *getToken(TokensStream *stream)
 {
     int i = stream->position;
+    int spaceCount = 0;
+    while(stream->source[i] == ' '){
+        ++spaceCount;
+        ++i;
+        if(spaceCount == 4){
+          stream->position = i;
+          return "\t";   
+        }
+    }
+    stream->position = i;
     if(stream->source[i] == '\0'){
         stream->position = i;
         return NULL;
@@ -45,16 +55,6 @@ const char *getToken(TokensStream *stream)
         return ")";
     }
 
-    int spaceCount = 0;
-    while(stream->source[i] == ' '){
-        ++spaceCount;
-        ++i;
-        if(spaceCount == 4){
-          stream->position = i;
-          return "\t";   
-        }
-    }
-    stream->position = i;
 
     bool found = false;
     while(!found){
@@ -77,7 +77,42 @@ const char *getToken(TokensStream *stream)
     return t;
 }
 
+bool isEndOfStream(TokensStream *ts)
+{
+    return ts->source[ts->position] == '\0';
+}
 
+Node parse(TokensStream *ts){
+    const char *token = getToken(ts);
+    Node res = {0, 0, 0};
+
+    res.name = token;
+    while(!isEndOfStream(ts)){
+        token = getToken(ts);
+        if(strcmp(token, ")") == 0) break;
+        res.childs_length++;
+        res.childs = (Node *)realloc(res.childs, res.childs_length * sizeof(Node));
+        if(strcmp(token, "(") != 0){
+            res.childs[res.childs_length - 1].name = token;
+            res.childs[res.childs_length - 1].childs_length = 0;
+        }
+        else
+            res.childs[res.childs_length - 1] = parse(ts);
+    }
+
+    return res;
+}
+
+void printTree(Node node, int shift)
+{
+    for(int i = 0; i < shift; i++)
+        printf("  ");
+    printf("%s\n", node.name);
+    for(int i = 0; i < node.childs_length; i++)
+        printTree(node.childs[i], shift + 1);
+}
+
+//TESTS
 void testGetToken(const char *source){
   TokensStream ts = {source, 0};
   const char *tk;
@@ -85,10 +120,18 @@ void testGetToken(const char *source){
       printf("[%s]\n", tk);
 }
 
+void testParseFirst(const char *source)
+{
+    TokensStream ts = {source, 0};
+    Node head = parse(&ts);
+    printTree(head, 0);
+}
+
 int main(int argc, char **argv)
 {
-    testGetToken("   def    say\n\tdo");
-    testGetToken("(def func (+ 10 11))");
+    //testGetToken("   def    say\n\tdo");
+    //testGetToken("(def func (+ 10 11))");
+    testParseFirst("def func (+ 10 11)");
     return 0;
 }
 
