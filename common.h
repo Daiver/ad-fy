a705cc718d3d1f4db802fd68bf84760d4013dff2
Yau Ref <yau.ref@gmail.com>
2013-10-25 03:03:09 +0400

    Common splitted to h and c files

--- common.h
+++ common.h
@@ -5,39 +5,14 @@
 #define true 1
 #define false !true
 
-#include<stdio.h>
-#include<unistd.h>
-#include<sys/stat.h>
-#include<malloc.h>
-#include<string.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <malloc.h>
+#include <string.h>
 
-bool isDigit(const char *s){
-    const char *str = s;
-    if(*str == '-'){
-        str++;
-        if(*str == '\0')
-            return false;
-    }
-    while (*str != '\0'){
-        if (*str < 48 || *str > 57) 
-            return false;
-        str++;
-    }
-    return true;
-}
+bool isDigit(const char *s);
 
-char *readFileAsLine(char *input_file_name){
-    char *file_contents;
-    long input_file_size;
-    FILE *input_file = fopen(input_file_name, "rb");
-    fseek(input_file, 0, SEEK_END);
-    input_file_size = ftell(input_file);
-    rewind(input_file);
-    file_contents = malloc((input_file_size) * (sizeof(char)));
-    fread(file_contents, sizeof(char), input_file_size, input_file);
-    fclose(input_file);
-    //file_contents[input_file_size] = 0;
-    return file_contents;
-}
+char *readFileAsLine(char *input_file_name);
 
 #endif
