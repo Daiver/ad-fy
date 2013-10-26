22c213ba6315c9b58bbf3640ef6707b4ee067413
Yau Ref <yau.ref@gmail.com>
2013-10-26 17:31:09 +0400

    Lexer refactored

--- lexer.c
+++ lexer.c
@@ -4,53 +4,41 @@
 #include "logging.h"
 #include "lexer.h"
 
-const char *stopchars[] = {"\0", "\t", "\n", "(", ")"};
+const char *stopchars[] = {" ", "\0", "\t", "\n", "(", ")"};
 const int stopchars_length = sizeof(stopchars)/sizeof(char *);
 
-const char *stopChar(char c, bool *isstopch){
+bool isStopChar(char c, char **ch){
   for(int i = 0; i < stopchars_length; ++i){
     if(*stopchars[i] == c){
-        *isstopch = true;
-	return stopchars[i];
+	*ch = stopchars[i];
+        return true;
     }
   }
-  isstopch = false;
-  return NULL;
+  return false;
 }
 
 const char *getToken(CodeStream *stream){
-    int i = stream->position;
     int spaceCount = 0;
-    while(stream->source[i] == ' '){
+    while(stream->source[stream->position] == ' '){
         ++spaceCount;
-        ++i;
-        if(spaceCount == 4){
-          stream->position = i;
+        ++stream->position;
+        if(spaceCount == 4)
           return "\t";   
-        }
     }
-    stream->position = i;
-    bool isstopch = false;
-    const char *ch = stopChar(stream->source[i], &isstopch);
-    if(isstopch){
-        stream->position = i + (ch != '\0');
+    char *ch = NULL;
+    if(isStopChar(stream->source[stream->position], &ch)){
+        stream->position += (ch != '\0');
         return ch;
     }
-    bool found = false;
-    while(!found){
-      stopChar(stream->source[i], &isstopch);
-      found = isstopch || stream->source[i] == ' ';
-      if(!found)
+    int i = stream->position;
+    while(!isStopChar(stream->source[i], &ch))
         ++i;
-    }
     int len = i - stream->position;
-    if(len + stream->position >= strlen(stream->source))
-        len = strlen(stream->source) - stream->position - 1;
-    char *t = (char *) malloc((len+1) * sizeof(char));
-    strncpy(t, stream->source + stream->position, len);    
-    t[len] = '\0';
+    char *token = (char *) malloc((len + 1) * sizeof(char));
+    strncpy(token, stream->source + stream->position, len);    
+    token[len] = '\0';
     stream->position = i;
-    return t;
+    return token;
 }
 
 bool isEndOfCode(CodeStream *ts){
