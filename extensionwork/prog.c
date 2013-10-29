37d476c67e5d6e17d5cdbd1cda7221c1acf0d430
Daiver <ra22341@ya.ru>
2013-10-30 00:23:11 +0400

    a few ext sample added

--- extensionwork/prog.c
+++ extensionwork/prog.c
@@ -4,7 +4,6 @@
 
 int main(int argc, char **argv) 
 {
-   void *lib_handle;
    int x;
    char *error;
    ExtContentGetter fn;
@@ -12,31 +11,39 @@ int main(int argc, char **argv)
         "/home/daiver/coding/ad-fy/extensionwork/bin/libext1.so.1.0",
         "/home/daiver/coding/ad-fy/extensionwork/bin/libext2.so.1.0"
    };
-   lib_handle = dlopen(ext_names[1] , RTLD_LAZY);
-   if (!lib_handle) 
-   {
-        fprintf(stderr, "%s\n", dlerror());
-        return 1;
-   }
+   int ext_num = 2;
+   void **lib_handle;
+   lib_handle = malloc(sizeof(void *) * ext_num);
 
-   fn = dlsym(lib_handle, "getExtContent");
-   if ((error = dlerror()) != NULL)  
-   {
-        fprintf(stderr, "%s\n", error);
-        return 1;
-   }
+   for (int ext_index = 0; ext_index < ext_num; ext_index++){
+       lib_handle[ext_index] = dlopen(ext_names[ext_index] , RTLD_LAZY);
+       if (!lib_handle) 
+       {
+            fprintf(stderr, "%s\n", dlerror());
+            return 1;
+       }
 
-   const char **names;
-   int len;
-   fn(&names, &len);
-   printf("len %d\n", len);
-   for(int i = 0; i < len; i += 2){
-        printf("[%d] %s - %s\n", i, names[i], names[i + 1]);
-        ExtBuiltInFunction func;
-        func = dlsym(lib_handle, names[i + 1]);
-        printf(">%d\n", func(10));
-   }
+       fn = dlsym(lib_handle[ext_index], "getExtContent");
+       if ((error = dlerror()) != NULL)  
+       {
+            fprintf(stderr, "%s\n", error);
+            return 1;
+       }
 
-   dlclose(lib_handle);
+       const char **names;
+       int len;
+       fn(&names, &len);
+       printf("len %d\n", len);
+       for(int i = 0; i < len; i += 2){
+            printf("[%d] %s - %s\n", i, names[i], names[i + 1]);
+            ExtBuiltInFunction func;
+            func = dlsym(lib_handle[ext_index], names[i + 1]);
+            printf(">%d\n", func(10));
+       }
+   }
+   for(int i = 0; i < ext_num; i++){
+       dlclose(lib_handle[i]);
+   }
+   free(lib_handle);
    return 0;
 }
