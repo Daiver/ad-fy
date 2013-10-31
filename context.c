3252dcc81d996638006a6d57ad099daff2d3ca5b
Yau Ref <yau.ref@gmail.com>
2013-11-01 02:45:10 +0400

    Added context feature - ignore local scope

--- context.c
+++ context.c
@@ -33,15 +33,16 @@ void context_set(Context *context, char *key, void *value, bool localy){
         return;
     hashtable_t *scope = NULL;
     hashtable_t *localScope = stack_pick(&context->scopes);
-    
     if(localy){
         scope = localScope;
     }else{
+        hashtable_t *tmp = stack_pop(&context->scopes);
         scope = findScope(context, key);
+        stack_push(&context->scopes, tmp);
         if(!scope)
             scope = localScope;
     }
-    ht_set(scope, key, value);
+    ht_set(scope, key, (char *)value);
 }
 
 void context_enterScope(Context *context){
@@ -71,32 +72,40 @@ void context_remove(Context *context){
         free(context);
     }
 }
-
+/*
 int main(){
     char *val = NULL;
     Context *context = context_new();
 
     context_enterScope(context);
     context_set(context, "a", "valA", true);
+    context_set(context, "b", "tmpValB", true);
     context_set(context, "b", "valB", true);
     val = (char*) context_get(context, "b");
-    printf("%s\n", val);
+    printf("%s\n", val ? val : "Not found");
+    val = (char*) context_get(context, "a");
+    printf("%s\n", val ? val : "Not found");
+    printf("\n");
 
     context_enterScope(context);
     context_set(context, "c", "valC", true);
     val = (char*) context_get(context, "a");
-    printf("%s\n", val);
+    printf("%s\n", val ? val : "Not found");
     context_set(context, "a", "localValA", true);
     val = (char*) context_get(context, "c");
-    printf("%s\n", val);
+    printf("%s\n", val ? val : "Not found");
     val = (char*) context_get(context, "a");
-    printf("%s\n", val);
+    printf("%s\n", val ? val : "Not found");
+    context_set(context, "a", "newValA", false);
+    val = (char*) context_get(context, "a");
+    printf("%s\n", val ? val : "Not found");
+    printf("\n");
 
     context_leaveScope(context);
     val = (char*) context_get(context, "a");
-    printf("%s\n", val);
+    printf("%s\n", val ? val : "Not found");
     val = (char*) context_get(context, "b");
-    printf("%s\n", val);
+    printf("%s\n", val ? val : "Not found");
     val = (char*) context_get(context, "c");
     printf("%s\n", val ? val : "Not found");
 
@@ -104,4 +113,4 @@ int main(){
     context_leaveScope(context);
     context_remove(context);
     return 0;
-}
+}*/
