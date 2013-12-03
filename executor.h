3b244c1510aff4e7f17906bc5ed8638514a7d387
Daiver <ra22341@ya.ru>
2013-12-03 14:48:30 +0400

    Removed commented lines  from executor

--- executor.h
+++ executor.h
@@ -8,38 +8,6 @@
 #include "context.h"
 #include "eapi.h"
 
-/*
-#define NTYPE_NONE 0
-#define NTYPE_BUILTIN_FUNC 1
-#define NTYPE_NODE 2
-#define NTYPE_FUNC 3
-#define NTYPE_INT 101
-#define NTYPE_BOOL 105
-#define NTYPE_LIST 110
-
-struct TObjectNode{
-    unsigned char type;
-    void *value;
-};
-typedef struct TObjectNode ObjectNode;
-
-struct TFunctionObj{
-    int args_length;
-    const char *prefix;
-    char **args;
-    int node_length;
-    Node **nodes;
-    Context *context;
-};
-typedef struct TFunctionObj FunctionObj;
-
-struct TObjectList{
-    int length;
-    ObjectNode *items;
-};
-typedef struct TObjectList ObjectList;
-*/
-
 ObjectNode *newObjectNode(unsigned char type, void *value);
 ObjectList *newObjectList(int length, ObjectNode *items);
 ObjectNode *execute(Context *context, Node *node);
